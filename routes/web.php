ade556a066b743ec3b4d203f2db544bd794370c5
Guido Cella <guido@guidocella.xyz>
2021-06-09 11:57:41 +0200

    use ::class

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use GuidoCella\LangFileManagerController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(array_merge(['web', 'auth'], config('lang_file_manager.middleware')))
-     ->prefix('admin/lang')
-     ->namespace('GuidoCella')
-     ->group(function () {
-         Route::get('', 'LangFileManagerController@index');
-         Route::get('{locale}/{group}', 'LangFileManagerController@edit');
-         Route::post('{locale}/{group}', 'LangFileManagerController@update');
-     });
+    ->prefix('admin/lang')
+    ->group(function () {
+        Route::get('', [LangFileManagerController::class, 'index']);
+        Route::get('{locale}/{group}', [LangFileManagerController::class, 'edit']);
+        Route::post('{locale}/{group}', [LangFileManagerController::class, 'edit']);
+    })
+;
