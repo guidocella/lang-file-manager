22ebed2361c970a41f8894c22953009eb593ba07
Guido Cella <guido@guidocella.xyz>
2024-04-26 11:11:28 +0200

    update code style

--- src/LangFileManagerServiceProvider.php
+++ src/LangFileManagerServiceProvider.php
@@ -9,23 +9,20 @@ class LangFileManagerServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->publishes([__DIR__ . '/../config/lang_file_manager.php' => config_path('lang_file_manager.php')], 'lang-file-manager');
+        $this->publishes([__DIR__.'/../config/lang_file_manager.php' => config_path('lang_file_manager.php')], 'lang-file-manager');
 
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'lang-file-manager');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'lang-file-manager');
 
-
-        /**
-         * Store an array in a config or lang file.
-         */
+        // Store an array in a config or lang file.
         File::macro('putArray', function (string $path, array $data) {
-            return $this->put("$path.php", "<?php\n\nreturn " . var_export($data, true) . ";\n");
+            return $this->put("$path.php", "<?php\n\nreturn ".var_export($data, true).";\n");
         });
     }
 
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/lang_file_manager.php', 'lang_file_manager');
+        $this->mergeConfigFrom(__DIR__.'/../config/lang_file_manager.php', 'lang_file_manager');
     }
 }
